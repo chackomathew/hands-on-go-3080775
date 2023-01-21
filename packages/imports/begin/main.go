0312022929f90077a15dd2f5ce900cdd1b25e012
Chacko Mathew <chackomathew@live.in>
2023-01-21 16:30:35 +0000

    study Go ch 1.2

--- packages/imports/begin/main.go
+++ packages/imports/begin/main.go
@@ -2,11 +2,22 @@
 package main
 
 // import the fmt package from the standard library
+// import (
+// 	"fmt"
+// 	"time"
+// )
+import (
+	"fmt"
+	"time"
+)
 
 // import the time package from the standard library
 
 func main() {
 	// use the fmt package to print the string "Hello Gopher!"
+	fmt.Println("Hello Gopher!")
 
 	// use the time package to print the current weekday
+	fmt.Println("Today is", time.Now().Weekday())
+	fmt.Printf("Today is %s\n", time.Now().Weekday())
 }
